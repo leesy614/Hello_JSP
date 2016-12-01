cb77053886c8bd1cceb0f21ff63427f724eef70c
leesy614 <deathcrow77@naver.com>
2016-12-01 10:12:26 +0900

    modify >> Hello.java

--- Hello_JSP/src/com/test/hello/Hello.java
+++ Hello_JSP/src/com/test/hello/Hello.java
@@ -1,12 +1,28 @@
-package com.test.hello;
+ package com.test.hello;
 
 public class Hello {
-
 	public static void main(String[] args) {
-		System.out.println("local Repository create Hello_JSP");
-       // 012456:hotfix edit by Lee
-         System.out.println("hotfix edit/modify");
-       // 012456:end  
-	}
+		System.out.println(&quot;local Repository create Hello_jsp&quot;);
+		// 012456:hotfix edit by LeePyeongjae
+		System.out.println(&quot;hotfix edit/modify&quot;);
+		// 012456:end  a
 
+		//0215238:origin/master edit by LeePyeongjae
+		System.out.println(&quot;Github edit&quot;);
+		
+		//031524:local/master edit by LeePyeongjae
+		System.out.println(&quot;local edit 1234&quot;);		
+		
+		//0412518:origin/master edit by Lee
+		System.out.println(&quot;Github edit 4567&quot;);
+		//0412518 end
+		
+		//0278944:pull by LeePyeongJae
+		System.out.println(&quot;Github edit 4567891&quot;);
+		//0278944
+		
+		
+		
+
+	}
 }
