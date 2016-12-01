96d8af0ff0bd5d3df2bc2594f9fe7354935b6a3a
Lee SY <deathcrow77@naver.com>
2016-12-01 09:08:23 +0900

    test

--- Hello_JSP/src/com/test/hello/Hello.java
+++ Hello_JSP/src/com/test/hello/Hello.java
@@ -6,7 +6,10 @@ public class Hello {
 		System.out.println("local Repository create Hello_JSP");
        // 012456:hotfix edit by Lee
          System.out.println("hotfix edit/modify");
-       // 012456:end  
+       // 012456:end 
+       
+       // 01235456:origin/master edit by Lee
+	System.out.println("Github edit");
 	}
 
 }
